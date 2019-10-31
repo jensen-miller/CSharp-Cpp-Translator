e451f72d8548ea5669221fe5f14f2cea0e9da632
Jensen Miller <millerjamtime@hotmail.com>
2019-10-31 01:44:22 -0400

    Begin Serial Sample

---
+++ SerialSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO.Ports;
+using System.Threading;
+
+//  Default arduino 'Serial' -> 'COM1'
+//  Default arduino baudrate -> 9600
+
+namespace SerialSample
+{
+    class Program
+    {
+        public static void Main()
+        {
+            const int defaultBaudRate = 9600;
+            const string defaultPort = "COM1";
+
+            SerialPort sp = new SerialPort(defaultPort, defaultBaudRate);         
+            
+            sp.Open();
+
+            while (true)
+            {
+                sp.WriteLine("Hello World!");
+                Thread.Sleep(3000);
+            }
+        }
+    }
+}
